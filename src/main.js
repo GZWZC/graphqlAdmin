27fe42d2a984bf037c030b8e62880ce3379ee584
wang_zhi_chao <wangzhichao@zuzuche.com>
2020-04-17 15:41:53 +0800

    demo

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import VueApollo from 'vue-apollo'
 
 import Cookies from 'js-cookie'
 
@@ -12,12 +13,17 @@ import '@/styles/index.scss' // global css
 import App from './App'
 import store from './store'
 import router from './router'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 
 import './icons' // icon
 import './permission' // permission control
 import './utils/error-log' // error log
 
 import * as filters from './filters' // global filters
+
+Vue.use(VueApollo)
 
 Vue.use(Element, {
   size: Cookies.get('size') || 'medium' // set element-ui default size
@@ -30,9 +36,29 @@ Object.keys(filters).forEach(key => {
 
 Vue.config.productionTip = false
 
+
+// 与 API 的 HTTP 连接
+const httpLink = createHttpLink({
+  // 你需要在这里使用绝对路径
+  uri: 'http://localhost:4000/graphql',
+})
+
+// 缓存实现
+const cache = new InMemoryCache()
+
+// 创建 apollo 客户端
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+})
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+})
+
 new Vue({
   el: '#app',
   router,
   store,
+  apolloProvider,
   render: h => h(App)
 })
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,16 +1,88 @@
 <template>
   <div class="dashboard-container">
-    123
+    <el-table
+      :data="list"
+      style="width: 100%">
+      <el-table-column
+        prop="id"
+        label="id"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="label"
+        label="label">
+      </el-table-column>
+    </el-table>
+    <el-button v-on:click="queryList()" style="margin-top: 20px;">queryList</el-button>
+    <el-button v-on:click="addList()" style="margin-top: 20px;">addList</el-button>
   </div>
 </template>
 
 <script>
-
+import gql from 'graphql-tag'
 export default {
   name: 'Dashboard',
   data() {
     return {
+      list: []
     }
+  },
+  methods: {
+    queryList() {
+      // 调用 graphql 变更
+      this.$apollo.query({
+        // 查询语句
+        query: gql`query list{
+          list {
+            id,
+            label
+          }
+        }`
+      }).then((data) => {
+        // 结果
+        this.data = res.data && res.data.list || []
+      })
+    },
+    addList() {
+      this.$apollo.mutate({
+        // 查询语句
+        mutation: gql`mutation ($label: String) {
+          addList(label: $label) {
+            id
+            label
+          }
+        }`,
+        update: (store, { data: { addList } }) => {
+          // 从缓存中读取这个查询的数据
+          const data = store.readQuery({ query: gql`query list{
+            list {
+              id,
+              label
+            }
+          }` })
+          // 将变更中的标签添加到最后
+          data.list.push(addList)
+          // 将数据写回缓存
+          store.writeQuery({ query: gql`query list{
+            list {
+              id,
+              label
+            }
+          }`, data })
+        },
+      }).then((data) => {
+        // 结果
+        console.log(data)
+      })
+    },
+  },
+  apollo: {
+    list: gql`query list{
+      list {
+        id,
+        label
+      }
+    }`
   }
 }
 </script>
